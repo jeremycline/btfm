jeremycline/btfm

commit c65f07b9054869290f658bad0ba29ada06551754

Skip processing if no words are detected in audio

src/voice.rs
@@ -383,6 +383,9 @@ impl VoiceEventHandler for Receiver {
                         let voice_data = user.reset().await;
                         let text =
                             voice_to_text(deepspeech_model, deepspeech_scorer, voice_data).await;
+                        if text.trim().len() == 0 {
+                            return None;
+                        }
                         let msg = format!("Bot heard \"{:}\"", &text);
                         log_event_to_channel(btfm_data.log_channel_id, &http_and_cache.http, &msg)
                             .await;

commit 7571cdb6ebd33a07fdba1cbd05b6a19d4031b32a

use is_empty() instead of len() == 0
clippy taught me the errors of my ways

Fixes: c65f07b90548

src/voice.rs
@@ -395,7 +395,7 @@ impl VoiceEventHandler for Receiver {
                         let voice_data = user.reset().await;
                         let text =
                             voice_to_text(deepspeech_model, deepspeech_scorer, voice_data).await;
-                        if text.trim().len() == 0 {
+                        if text.is_empty() {
                             return None;
                         }
                         let msg = format!("Bot heard \"{:}\"", &text);