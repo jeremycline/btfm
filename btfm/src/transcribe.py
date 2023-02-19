64365abb5155448d8058c6c61c26f66f5eecb3b4
Jeremy Cline <jeremy@jcline.org>
2023-02-19 14:50:23 -0500

    Rename btfm to btfm-server and btfm-cli to btfm
    Mostly we use the CLI so make that name nice and short.

--- btfm/src/transcribe.py
+++ btfm/src/transcribe.py
@@ -20,11 +20,12 @@ def transcribe(audio: Union[str, np.ndarray, torch.Tensor], fp16=False):
 
     audio: The path to audio or either a NumPy array or Tensor containing the
         audio. If it's not a path the audio must be mono f32 16 kHz format.
-    fp16: If your GPU supports FP16, you can set this to 'True' for better performance.
+    fp16: If your GPU supports FP16, you can set this to 'True' for better
+        performance.
     """
     text = ""
     if MODEL is None:
-        print("Programmer error, you must load the model first with 'load_model()'")
+        print("You must load the model first with 'load_model()'")
     else:
         try:
             result = whisper.transcribe(MODEL, audio, verbose=None, fp16=fp16)
